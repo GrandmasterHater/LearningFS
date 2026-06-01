65ba49f789b61b57b9a80ba1589a7f29c3c157a0
sguschin <sguschin@gear-games.com>
2026-06-01 19:37:47 +0400

    FS: Task7

---
+++ HW/Task7/Task7.fs
@@ -0,0 +1,21 @@
+﻿// 20.3.1
+let vat n x =
+    if n < 0 || n > 100 then
+        -1.0
+    else
+        x + (x * float(n)) / 100.0
+
+// 20.3.2
+let unvat n x =
+    if n < 0 || n > 100 then
+        -1.0
+    else
+        x / vat n 1.0
+
+// 20.3.3
+let rec min f =
+    let rec find_n_rec = function
+       | (f, n) when f(n) = 0 -> n
+       | (f, n) -> find_n_rec(f, n + 1)
+    
+    find_n_rec(f, 1)
