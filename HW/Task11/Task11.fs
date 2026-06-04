8f23ed0d57f99d604f65768783463032de1c87f2
sguschin <sguschin@gear-games.com>
2026-06-04 18:32:29 +0400

    FS: Task 11

---
+++ HW/Task11/Task11.fs
@@ -0,0 +1,16 @@
+﻿type F = 
+  | AM
+  | PM
+
+type TimeOfDay = { hours : int; minutes : int; f: F }
+
+let (.>.) x y =
+    let to_minutes { hours = h; minutes = m; f = fd } =
+        let calc_by_day_part = function
+            | AM -> h * 60 + m
+            | PM -> (h + 12) * 60 + m
+            
+        calc_by_day_part fd
+    
+    to_minutes x > to_minutes y
+
