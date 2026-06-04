4ba536f6e0e4bf8907d1589a8f36e388c8610c08
sguschin <sguschin@gear-games.com>
2026-06-04 15:47:01 +0400

    FS: Task10 - fix syntax

--- HW/Task10/Task10.fs
+++ HW/Task10/Task10.fs
@@ -1,13 +1,10 @@
 ﻿type TimeOfDay = { hours: int; minutes: int; f: string }
 
-let to_minutes { hours = h; minutes = m; f = fd } =
-    let half_day_minutes = if fd = "AM" then 0 else 12 * 60
-    h * 60 + m + half_day_minutes
-
 let (.>.) x y =
+    let to_minutes { hours = h; minutes = m; f = fd } =
+        let half_day_minutes = if fd = "AM" then 0 else 12 * 60
+        h * 60 + m + half_day_minutes
+    
     to_minutes x > to_minutes y
-    
-    
-    
     
     
