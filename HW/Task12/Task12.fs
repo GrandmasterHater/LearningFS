2c03d018a51b2978f08278b35c03d2f047b6f228
sguschin <sguschin@gear-games.com>
2026-06-05 18:09:54 +0400

    FS: Task12 - improve logic

--- HW/Task12/Task12.fs
+++ HW/Task12/Task12.fs
@@ -1,26 +1,20 @@
-﻿// 34.1
-let rec upto n =
-    let rec iterate_from = function
-        | (i, n) when n <= 0 -> []
-        | (i, n) when i = n -> [n]
-        | (i, n) -> i :: iterate_from (i + 1, n)
-    
-    iterate_from (1, n)
+﻿let rec iterate_from_to_step = function
+        | (f, t, s) when s = 0 -> []
+        | (f, t, s) when f < t && s < 0 -> []
+        | (f, t, s) when f > t && s > 0 -> []
+        | (f, t, s) when f = t -> [t]
+        | (f, t, s) -> f :: iterate_from_to_step (f + s, t, s)
+
+// 34.1
+let rec upto n= iterate_from_to_step (1, n, 1)
 
 // 34.2
-let rec dnto = function
-    | n when n <= 0 -> []
-    | n -> n :: dnto(n - 1)
+let rec dnto n = iterate_from_to_step (n, 1, -1)
 
 // 34.3
-let rec evenn n =
-    let rec collect_even_numbers = function
-        | (i, n) when n < 0 -> []
-        | (i, n) when i = n -> [n]
-        | (i, n) -> i :: collect_even_numbers (i + 2, n)
-        
-    let calc_start_value n = (0, n * 2 - 2)
-    let get_even = calc_start_value >> collect_even_numbers
+let rec evenn n =   
+    let calc_start_value n = (0, n * 2 - 2, 2)
+    let get_even = calc_start_value >> iterate_from_to_step
     
     get_even n
 
