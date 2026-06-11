9d4c11d0daa8462bd81ba9ce2372a4a6dfd1d9b8
sguschin <sguschin@gear-games.com>
2026-06-11 13:58:23 +0400

    FS: Task15

---
+++ HW/Task15/Task15.fs
@@ -0,0 +1,11 @@
+﻿// 41.4.1
+let list_filter f xs = List.foldBack (fun i acc -> if f(i) then i :: acc else acc) xs []
+
+// 41.4.2
+let sum (p, xs) = List.fold (fun acc i -> if p(i) then i + acc else acc) 0 xs
+
+// 41.4.3
+let revrev lst =
+    let rev l = List.fold (fun acc i -> i :: acc) [] l
+    List.fold (fun acc i -> rev i :: acc) [] lst
+
