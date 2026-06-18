ebe0d25754da502f7d2ffb0673d0832278323e41
sguschin <sguschin@gear-games.com>
2026-06-18 20:11:55 +0400

    FS: Task20

---
+++ HW/Task20/Task20.fs
@@ -0,0 +1,16 @@
+﻿// 49.5.1
+let even_seq = Seq.initInfinite (fun i -> i * 2)
+
+// 49.5.2
+let fac_seq =
+    let rec fact f n =
+        match n with 
+            | n when n <= 0 -> f 1
+            | n -> fact (fun x -> f (n * x)) (n - 1)
+        
+    Seq.initInfinite (fact id)
+        
+
+// 49.5.3
+let seq_seq = Seq.initInfinite (fun i -> if i = 0 then Seq.singleton 0 else Seq.ofList [-i; i]) |> Seq.concat 
+
