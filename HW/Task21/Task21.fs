c29a2c78a07da6aa2983d318f1e72f64066ee794
sguschin <sguschin@gear-games.com>
2026-06-19 13:15:40 +0400

    FS: Task21

---
+++ HW/Task21/Task21.fs
@@ -0,0 +1,25 @@
+﻿// 50.2.1
+let fac_seq = seq {
+        let mutable fact = 1
+        
+        yield fact
+        
+        let mutable i = 1
+        
+        while true do
+            fact <- fact * i
+            yield fact
+            i <- i + 1
+    }
+
+// 50.2.2
+let seq_seq = seq {
+        yield 0
+        
+        let mutable i = 1
+        
+        while true do
+            yield -i
+            yield i
+            i <- i + 1
+    }
